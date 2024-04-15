16e97af2c35dac987b18eab13954db7b74e5dcf2
renato-farias <renato.santos@bbc.co.uk>
2024-04-16 00:44:46 +0100

    remove unsed stuff

--- src/moneywiz_api/managers/scheduled_transaction_manager.py
+++ src/moneywiz_api/managers/scheduled_transaction_manager.py
@@ -1,7 +1,5 @@
-from datetime import datetime
-from typing import Dict, Callable, List, Tuple
+from typing import Dict, Callable, List
 
-from moneywiz_api import utils
 from moneywiz_api.database_accessor import DatabaseAccessor
 from moneywiz_api.model.scheduled_transaction import (
     ScheduledTransaction,
@@ -11,15 +9,11 @@ from moneywiz_api.model.scheduled_transaction import (
     ScheduledWithdrawTransactionHandler,
 )
 from moneywiz_api.managers.record_manager import RecordManager
-from moneywiz_api.types import ID
 
 
 class ScheduledTransactionManager(RecordManager[ScheduledTransaction]):
     def __init__(self):
         super().__init__()
-        self.category_assignment: Dict[ID, List[Tuple[ID, float]]] = {}
-        self.refund_maps: Dict[ID, ID] = {}
-        self.tags_map: Dict[ID, ID] = {}
 
     @property
     def ents(self) -> Dict[str, Callable]:
@@ -32,24 +26,6 @@ class ScheduledTransactionManager(RecordManager[ScheduledTransaction]):
 
     def load(self, db_accessor: DatabaseAccessor) -> None:
         super().load(db_accessor)
-        self.category_assignment: Dict[ID, List[Tuple[ID, float]]] = (
-            db_accessor.get_category_assignment()
-        )
-        self.refund_maps: Dict[ID, ID] = db_accessor.get_refund_maps()
-        self.tags_map: Dict[ID, ID] = db_accessor.get_tags_map()
-
-    def category_for_transaction(
-        self, transaction_id: ID
-    ) -> List[Tuple[ID, float]] | None:
-        return self.category_assignment.get(transaction_id)
-
-    def tags_for_transaction(self, transaction_id: ID) -> List[ID] | None:
-        return self.tags_map.get(transaction_id)
-
-    def original_transaction_for_refund_transaction(
-        self, transaction_id: ID
-    ) -> ID | None:
-        return self.refund_maps.get(transaction_id)
 
     def get_all(self) -> List[ScheduledTransaction]:
         return sorted(
